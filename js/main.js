30585694b0849de18ce99658b6dbec7c9b65619d
fabiosuarez <fabim3158@gmail.com>
2016-02-17 22:26:57 -0200

    feat(navigationbar): Top Bar and Side Bar Navigation
    Adding styles and structures for the main navigation in the site

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage();
+    $('#fullpage').fullpage({
+        anchors:['section1', 'section2', 'section3', 'section4','section5','section6', 'section7'],
+        menu: '#sidenav'
+    });
 });
