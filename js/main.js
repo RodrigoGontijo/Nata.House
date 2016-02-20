99e5a909e870f5c5cd9eac07322da1304c9669d5
Vitor Boufleur <v.boufleur@gmail.com>
2016-02-19 23:43:33 -0200

    feat(): Added transition effects on the header title

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
+//This one executes when the DOM is ready to be manipulated
 $(document).ready(function() {
     $('#fullpage').fullpage({
         anchors:['section-title', 'section-about', 'section-services', 'section-people', 'section-contact','section6'],
         menu: '#sidenav',
         loopBottom: true,
+    });
+
+    //Title header transition
+    $('.layout-fade-in-down').addClass("layout-fade-in-down-complete");
+    $('.layout-fade-in-down').bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function() {
+      $('.layout-toggle-color').addClass("text-primary");
     });
 });
